46afa99fa3be5b69492adc9458859fddb149ad92
mwpo <mikegoodbest@sina.com>
2012-11-02 10:28:50 +0800

    Empty board

---
+++ GoBangGame/GoBangGame/GoBangBoard.cs
@@ -0,0 +1,24 @@
+﻿namespace GoBangGame
+{
+    public class GoBangBoard
+    {
+        public enum GridStatus
+        {
+            Empty,
+            Black,
+            White,
+        }
+
+        private GridStatus[,] grids;
+
+        public GoBangBoard(int rowCount, int colomnCount)
+        {
+            grids = new GridStatus[rowCount, colomnCount];   
+        }
+
+        public GridStatus this[int row, int col]
+        {
+            get { return grids[row, col]; }
+        }
+    }
+}
---
+++ GoBangGame/TestGobang/GoBangTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoBangGame;
+
+namespace TestGobang
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class GoBangTest
+    {
+        [TestMethod]
+        public void TestEmptyBoard()
+        {
+            var board = new GoBangBoard(8, 8);
+            
+            for(int i=0; i<8; ++i)
+                for(int j=0; j<8; ++j)
+                {
+                    Assert.IsTrue(board[i, j] == GoBangBoard.GridStatus.Empty);
+                }
+        }
+    }
+
+    
+}
